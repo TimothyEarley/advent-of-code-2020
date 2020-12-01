bb7e93619d5bfc99e460c9365546af5e16c76965
Timothy Earley <timmy@earley.de>
2020-12-01 15:32:25 +0100

    Project structure

---
+++ README.md
@@ -0,0 +1,4 @@
+Advent of Code 2020
+===================
+
+Solutions to the [Advent of Code](https://adventofcode.com/2020) written in Kotlin.
---
+++ build.gradle.kts
@@ -0,0 +1,15 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.4.20"
+}
+group = "de.earley"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+tasks.withType<KotlinCompile>() {
+    kotlinOptions.jvmTarget = "11"
+}
