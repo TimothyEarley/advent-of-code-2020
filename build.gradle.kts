916e77da800c9823baef6f5cc1be2ae74fbbd562
Timothy Earley <timmy@earley.de>
2024-12-09 00:19:51 +0100

    Updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.4.20"
+    kotlin("jvm") version "2.1.0"
 }
 group = "de.earley"
 version = "1.0-SNAPSHOT"
@@ -10,9 +10,6 @@ repositories {
     mavenCentral()
 }
 
-tasks.withType<KotlinCompile>() {
-    kotlinOptions {
-        jvmTarget = "11"
-        freeCompilerArgs += "-Xopt-in=kotlin.RequiresOptIn"
-    }
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
 }
